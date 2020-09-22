4d45b39b97d968dcb2dc9b21c40cfca52f8900e4
momchilmitev <momchilmitev87@gmail.com>
2020-09-22 10:12:25 +0200

    added @babel/plugin-transform-async-to-generator plugin to transfor async await in generator

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,32 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-transform-async-to-generator"],
             },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: [
-                    { 
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({ template: './src/index.html' })
+          },
+        ],
+      },
     ],
-    mode: 'development'
-}
+  },
+  plugins: [new HTMLWebpackPlugin({ template: "./src/index.html" })],
+  mode: "development",
+};
